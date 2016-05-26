5c4dfbfd3f5e6b590a011b00aa322417d479d502
Kirill Polunin <imbamrgrey@gmail.com>
2016-05-26 22:34:09 +0300

    Initial Commit

---
+++ BrainActivitySwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BrainActivitySwift
+//
+//  Created by Victor Gelmutdinov on 26/05/16.
+//  Copyright © 2016 Kirill Polunin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
