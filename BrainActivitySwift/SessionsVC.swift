2768bb429ee735ed95a75ecfd0b425031e79c4cd
Kirill Polunin <imbamrgrey@gmail.com>
2016-06-07 00:58:19 +0300

    Sessions accordion prototyping ready

--- BrainActivitySwift/SessionsVC.swift
+++ BrainActivitySwift/SessionsVC.swift
@@ -11,13 +11,20 @@ import UIKit
 class SessionsVC : UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        addAccordionSections()
+    }
+    
+    // MARK: - Helpers
+    func addAccordionSections()  {
         let width = self.view.bounds.width
         let height = self.view.bounds.height
         let accordion = AccordionView(frame: CGRectMake(0,70,width,height))
         self.view.addSubview(accordion)
-        let header = SessionHeaderView(dateLabel: "Tuesday",moodLabel: "zxczcx")
-        let section = SessionsContainerView(containerWidth: width)
-        accordion.addHeader(header, withView: section)
+        for day in ["Tuesday","Wednesday","Sunday"]{
+            let header = SessionHeaderView(dateLabel: day,moodLabel: day)
+            let section = SessionsContainerView(containerWidth: width)
+            accordion.addHeader(header, withView: section)
+        }
         accordion.setNeedsLayout()
         accordion.allowsMultipleSelection = true
         accordion.allowsEmptySelection = true
--- SessionsContainerView.swift
+++ SessionsContainerView.swift
@@ -8,18 +8,21 @@
 
 import UIKit
 
-class SessionsContainerView: UIView {
-    var resultsButton : UIView!
+class SessionsContainerView: UIView ,UICollectionViewDataSource{
+    var resultsButton = UIButton()
     var upperContainer : UIView!
-    var collectionView : UICollectionView!
     var clockImageView = UIImageView(image: UIImage(named: "clock-128"))
     var clockTimeLabel  = UILabel()
     var resultLabel  = UILabel()
+    var activityCollectionView : UICollectionView!
     override func updateConstraints() {
         super.updateConstraints()
         //upperContainer.translatesAutoresizingMaskIntoConstraints = false
         clockImageView.translatesAutoresizingMaskIntoConstraints = false
         clockTimeLabel.translatesAutoresizingMaskIntoConstraints = false
+        resultsButton.translatesAutoresizingMaskIntoConstraints = false
+        resultLabel.translatesAutoresizingMaskIntoConstraints = false
+        activityCollectionView.translatesAutoresizingMaskIntoConstraints = false
         //clock image
         self.addConstraint(NSLayoutConstraint(item: clockImageView, attribute: .CenterY, relatedBy: .Equal, toItem: upperContainer, attribute: .CenterY, multiplier: 1.0, constant: 0.0))
         self.addConstraint(NSLayoutConstraint(item: clockImageView, attribute: .LeadingMargin, relatedBy: .Equal, toItem: upperContainer, attribute: .LeadingMargin, multiplier: 1.0, constant: 0.0))
@@ -27,10 +30,14 @@ class SessionsContainerView: UIView {
         self.addConstraint(NSLayoutConstraint(item: clockImageView, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 30))
         
         self.Constraints(forTarget: clockTimeLabel).Related(to: clockImageView).CenterY(0).AddComplex(from: .Leading, to: .Trailing, value: 10).Height(20)
-        
+        self.Constraints(forTarget: resultsButton).Related(to: upperContainer).CenterY(0).Trailing(0).Height(30).Width(30)
+        self.Constraints(forTarget: resultLabel).Related(to: resultsButton).Height(20).Width(60).CenterY(0).AddComplex(from: .Trailing , to: .Leading, value: 10)
+        self.Constraints(forTarget: activityCollectionView).Related(to: self).Trailing(0).Leading(0).Bottom(0)
+        self.Constraints(forTarget: activityCollectionView).Related(to: upperContainer).AddComplex(from: .Top, to: .Bottom, value: 0)
     }
     init(containerWidth : CGFloat) {
-        super.init(frame: CGRectMake(0, 0, 0, 100))
+        let sectionHeight : CGFloat = 100.0
+        super.init(frame: CGRectMake(0, 0, 0, sectionHeight))
         //section
         self.backgroundColor = UIColor.whiteColor()
         self.layer.borderWidth = 2
@@ -39,14 +46,40 @@ class SessionsContainerView: UIView {
         upperContainer = UIView(frame: CGRectMake(0, 0, containerWidth,30))
         upperContainer.layer.borderWidth = 1
         upperContainer.layer.borderColor = UIColor.grayColor().CGColor
-        upperContainer.addSubview(clockImageView)
+
         clockTimeLabel.text = "18:34:19"
-        //self.addSubview(resultLabel)
+        resultsButton.setImage(UIImage(named: "diagram-icon"), forState: .Normal)
+        resultLabel.text = "results"
+        
+        upperContainer.addSubview(clockImageView)
+        upperContainer.addSubview(resultsButton)
+        upperContainer.addSubview(resultLabel)
+        //bottomcontainer
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+        layout.itemSize = CGSize(width: sectionHeight/2, height: sectionHeight/2)
+        activityCollectionView = UICollectionView(frame: CGRectMake(0, 0, 0, 0), collectionViewLayout: layout)
+     
+        activityCollectionView.dataSource = self
+        activityCollectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "reusableCellId")
+        activityCollectionView.backgroundColor = UIColor.whiteColor()
         self.addSubview(upperContainer)
         self.addSubview(clockTimeLabel)
+        self.addSubview(activityCollectionView)
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Collection View
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("reusableCellId", forIndexPath: indexPath)
+        cell.backgroundView = UIImageView(image: UIImage(named: "unnamed"))
+        return cell
     }
 }
